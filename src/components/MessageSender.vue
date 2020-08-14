13d3425ada1a7a433e5822ecad692e777f6131e5
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2020-08-14 18:40:49 +0100

    revert: dynamic message input height not worth it

--- src/components/MessageSender.vue
+++ src/components/MessageSender.vue
@@ -9,7 +9,7 @@
       </p>
       <div class="field">
         <div class="control">
-          <textarea class="textarea" placeholder="Enter message" v-model.trim="message" :rows="rows"></textarea>
+          <textarea class="textarea" placeholder="Enter message" v-model.trim="message" rows="8"></textarea>
         </div>
       </div>
       <div class="field">
@@ -28,7 +28,6 @@ export default {
     return {
       errors: [],
       message: null,
-      rows: 6,
       sender: null
     }
   },
@@ -45,8 +44,6 @@ export default {
         localStorage.setItem('message', this.message)
         this.sendMessage()
       }
-    },
-    messageInputHeight () {
     },
     async sendMessage () {
       if (!this.sender) {
@@ -68,18 +65,6 @@ export default {
         this.sender = this.qClient.createSender()
       } else {
         this.sender = null
-      }
-    },
-    message () {
-      console.log('message has changed')
-      if (this.message) {
-        try {
-          // add 3 to cater for the '{', '}' and a new line
-          this.rows = Object.keys(JSON.parse(this.message)).length + 3
-        } catch (err) {
-          // ignore error
-        }
-        return this.rows
       }
     }
   }
