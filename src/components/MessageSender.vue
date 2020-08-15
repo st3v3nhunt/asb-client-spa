d0990ee357e9a17cb530437a194878ef0dd20a61
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2020-08-15 01:34:38 +0100

    feat: list sent messages. add clear sent messages button

--- src/components/MessageSender.vue
+++ src/components/MessageSender.vue
@@ -1,23 +1,41 @@
 <template>
   <div>
-    <form @submit.prevent="checkForm">
-      <p v-if="errors.length">
-        <b>Please correct the following error(s):</b>
-        <ul>
-          <li v-for="error in errors" :key="error">{{ error }}</li>
-        </ul>
-      </p>
-      <div class="field">
-        <div class="control">
-          <textarea class="textarea" placeholder="Enter message" v-model.trim="message" rows="8"></textarea>
-        </div>
+    <div class="columns">
+      <div class="column">
+        <form @submit.prevent="checkForm">
+          <p v-if="errors.length">
+            <b>Please correct the following error(s):</b>
+            <ul>
+              <li v-for="error in errors" :key="error">{{ error }}</li>
+            </ul>
+          </p>
+          <div class="field">
+            <div class="control">
+              <textarea class="textarea" placeholder="Enter message" v-model.trim="message" rows="8"></textarea>
+            </div>
+          </div>
+          <div class="columns">
+            <div class="column">
+              <div class="control">
+                <button class="button is-success is-fullwidth" :disabled="!qClient">Send Message</button>
+              </div>
+            </div>
+            <div class="column">
+              <div class="control">
+                <button class="button is-warning is-fullwidth" :disabled="messages.length === 0" @click="clearMessages">Clear Sent Messages</button>
+              </div>
+            </div>
+          </div>
+        </form>
       </div>
-      <div class="field">
-        <div class="control">
-          <button class="button is-success" :disabled="!qClient">Send Message</button>
-        </div>
+    </div>
+    <div class="columns">
+      <div class="column">
+      <article v-for="(message, index) in messages" :key="index" class="message is-small">
+        <div class="message-body">{{ message }}</div>
+      </article>
       </div>
-    </form>
+    </div>
   </div>
 </template>
 
@@ -28,6 +46,7 @@ export default {
     return {
       errors: [],
       message: null,
+      messages: [],
       sender: null
     }
   },
@@ -45,10 +64,14 @@ export default {
         this.sendMessage()
       }
     },
+    clearMessages () {
+      this.messages = []
+    },
     async sendMessage () {
       if (!this.sender) {
         this.sender = this.qClient.createSender()
       }
+      this.messages.unshift(this.message)
       await this.sender.send({ body: JSON.parse(this.message) })
       console.log('Sending message', this.message)
     }
